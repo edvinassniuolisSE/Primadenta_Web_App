03a8a447520744e6f9cc6ecba57bb32c3ec677a6
Edvinas <edvinas.sniuolis@gmail.com>
2017-10-25 20:13:18 +0300

    Add project files.

---
+++ Primadenta_Web_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Primadenta_Web_App.Startup))]
+namespace Primadenta_Web_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
